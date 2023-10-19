ed6871a87028ddc2600d3430f69706e760e57846
Albert Ragány-Németh <me@alb1.hu>
2023-10-19 15:01:39 +0200

    Add docker deployment, update to .NET 8

--- Hello/deploy.ps1
+++ Hello/deploy.ps1
@@ -1,15 +1,15 @@
 $PrevDir = $Pwd
+cd $PSScriptRoot/..
 
-cd $PSScriptRoot
-
-Get-ChildItem -include bin,obj,.vs -recurse -force | Remove-Item -recurse -force
-
-if (dotnet publish -c Release -r linux-x64 --self-contained) {
-    ssh albi@iron 'systemctl --user stop alb1.hu'
-
-    scp -r bin/Release/net7.0/linux-x64/publish/* albi@iron:/home/albi/www/alb1.hu/bin
-
-    ssh albi@iron 'systemctl --user restart alb1.hu'
+try {
+    docker build . -t alb1 -f ./Hello/Dockerfile
+    mkdir -f -p ./Hello/bin
+    docker save alb1 -o ./Hello/bin/alb1.tar
+    $tmp = $(ssh albi@iron 'mktemp -d')
+    scp -C ./Hello/bin/alb1.tar albi@iron:$tmp
+    ssh root@iron "docker load -i $tmp/alb1.tar"
+    ssh root@iron 'systemctl restart docker-alb1.service'
 }
-
-cd $PrevDir
+finally {
+    cd $PrevDir
+}
